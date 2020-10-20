131033686e111fbbf002665531fffc55b7cb5ef6
Julian Rivera Jr <julianriverajr@gmail.com>
2020-10-20 14:09:24 -0500

    var to const

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 8080;
+const app = express();
+const PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
