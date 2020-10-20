b29bdb504566b15b0b8f4d88481cbc22abb68fc0
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-10-20 14:10:11 -0500

    changed var to const

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 8080;
+const app = express();
+const PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const reservations = [];
+
+app.get("/api/reservations", function(req, res) {
+    return.json(characters);
+}
