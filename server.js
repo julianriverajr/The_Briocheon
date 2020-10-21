58399cf79eefd41de12e63290d078321daeb3ce5
Umut Alptekin <amacalptekin@gmail.com>
2020-10-21 18:47:48 +0300

    added app.get blocks for all html files

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ const reservations = [];
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/home.html"));
-  });
+});
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/reserve.html"));
+});
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/tables.html"));
+});
 
 app.get("/api/reservations", function(req, res) {
     return res.json(reservations);
